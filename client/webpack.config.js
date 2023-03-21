c4ee7f9a99ce38f7e9039a03a6046879158317af
Dave Estey <daveestey.working@gmail.com>
2023-03-21 18:22:49 -0300

    added the workbox plugin and service worker abd manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const { title } = require('process');
 
 module.exports = () => {
   return {
@@ -17,13 +15,41 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // configure workbox plugins for a service worker and manifest file.
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
       
-    ],
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'J.A.T.E',
+        short_name: 'J.A.T.E',
+        description: 'Your simplified text editor!',
+        background_color: '#003366',
+        theme_color: '#FFFF00',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    // CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+
       ],
     },
   };
