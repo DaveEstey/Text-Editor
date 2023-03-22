6e3d072e3243a704740d12413498eac51a7d89fa
Dave Estey <daveestey.working@gmail.com>
2023-03-21 21:07:24 -0300

    added the get and put functionality to the DB database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+const jateDb = await openDB('jate', 1);
+jateDb.transaction('jate', 'readwrite')
+  .objectStore('jate')
+  .add([{content}]);
+  
+const result = await jateDb;
+
+console.log(result, ' was saved to openDB')
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
 
+  const jateDb = await openDB('jate', 1);
+  jateDb.transaction('jate', 'readonly')
+    .objectStore('jate')
+    .getAll();
+    
+  const result = await jateDb;
+  
+  console.log('result from DB ', result);
+  }
+  
 initdb();
